5085075d0dea0347e47c643e1084b7cb513e4b07
Uitterdijk, Niels <niels.uitterdijk@zuhlke.com>
2024-04-12 14:12:41 +0200

    fix: typo

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,7 +1,7 @@
 from app.views import ListCreateObjects, RetrieveUpdateDestroyObjects
 from django.urls import path
 
-urlpatternsgit = [
+urlpatterns = [
     path("objects", ListCreateObjects.as_view()),
     path("objects/<int:pk>", RetrieveUpdateDestroyObjects.as_view()),
 ]
